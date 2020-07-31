8ae6a961c22e1ce3567e84ad890a441d1c96975e
Rafid MD. Ahsan <rafidahsan698@gmail.com>
2020-07-31 14:58:32 +0600

    finishing front end

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -160,7 +160,104 @@
                 <a href="#" class="block text-base font-semibold leading-tight hover:gray-400 mt-8">NFS Most Wanted</a>
                 <div class="text-gray-400 mt-1">PC/Desktop</div>
             </div>
+        </div>
+         {{-- end popular games --}}
+         <div class="flex my-10">
+            <div class="recently-reviews w-3/4 mr-32">
+                <h2 class="text-red-600 uppercase tracking-wide font-semibold">Recently Reviewed</h2>
+                <div class="recently-reviewed-container space-y-12 mt-8">
+                    {{-- recently reviewed container starts --}}
+                    <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
+                        <div class="relative flex-none">
+                            <a href="#">
+                                <img src="/assets/img/postar.jpg" alt="cover" class="w-48 hover:opacity-75 transition ease-in-out duration-150">
+                            </a>
+                            <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full" style="right: -20px; bottom: -20px;">
+                                <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
+                            </div>
+                        </div>
+                        <div class="ml-12">
+                            <a href="#" class="block text-lg font-semibold leading-tight hover:gray-400">NFS Most Wanted</a>
+                            <div class="text-gray-400 mt-1">PC/Desktop</div>
+                            <div class="mt-6 text-gray-400">
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit adipisci quasi nulla. Sed temporibus quas, totam ullam reprehenderit ipsa minus quaerat veniam unde, quam doloremque recusandae fugiat! Ipsa, quidem? A fugiat velit quos earum? Natus consectetur nulla recusandae doloribus eius!
+                            </div>
+                        </div>
+                    </div>
 
+                    <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
+                        <div class="relative flex-none">
+                            <a href="#">
+                                <img src="/assets/img/postar.jpg" alt="cover" class="w-48 hover:opacity-75 transition ease-in-out duration-150">
+                            </a>
+                            <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full" style="right: -20px; bottom: -20px;">
+                                <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
+                            </div>
+                        </div>
+                        <div class="ml-12">
+                            <a href="#" class="block text-lg font-semibold leading-tight hover:gray-400">NFS Most Wanted</a>
+                            <div class="text-gray-400 mt-1">PC/Desktop</div>
+                            <div class="mt-6 text-gray-400">
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit adipisci quasi nulla. Sed temporibus quas, totam ullam reprehenderit ipsa minus quaerat veniam unde, quam doloremque recusandae fugiat! Ipsa, quidem? A fugiat velit quos earum? Natus consectetur nulla recusandae doloribus eius!
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
+                        <div class="relative flex-none">
+                            <a href="#">
+                                <img src="/assets/img/postar.jpg" alt="cover" class="w-48 hover:opacity-75 transition ease-in-out duration-150">
+                            </a>
+                            <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full" style="right: -20px; bottom: -20px;">
+                                <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
+                            </div>
+                        </div>
+                        <div class="ml-12">
+                            <a href="#" class="block text-lg font-semibold leading-tight hover:gray-400">NFS Most Wanted</a>
+                            <div class="text-gray-400 mt-1">PC/Desktop</div>
+                            <div class="mt-6 text-gray-400">
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit adipisci quasi nulla. Sed temporibus quas, totam ullam reprehenderit ipsa minus quaerat veniam unde, quam doloremque recusandae fugiat! Ipsa, quidem? A fugiat velit quos earum? Natus consectetur nulla recusandae doloribus eius!
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
+                        <div class="relative flex-none">
+                            <a href="#">
+                                <img src="/assets/img/postar.jpg" alt="cover" class="w-48 hover:opacity-75 transition ease-in-out duration-150">
+                            </a>
+                            <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full" style="right: -20px; bottom: -20px;">
+                                <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
+                            </div>
+                        </div>
+                        <div class="ml-12">
+                            <a href="#" class="block text-lg font-semibold leading-tight hover:gray-400">NFS Most Wanted</a>
+                            <div class="text-gray-400 mt-1">PC/Desktop</div>
+                            <div class="mt-6 text-gray-400">
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit adipisci quasi nulla. Sed temporibus quas, totam ullam reprehenderit ipsa minus quaerat veniam unde, quam doloremque recusandae fugiat! Ipsa, quidem? A fugiat velit quos earum? Natus consectetur nulla recusandae doloribus eius!
+                            </div>
+                        </div>
+                    </div>
+                    {{-- recently reviewed container ends --}}
+                </div>
+            </div>
+
+            {{-- most-anticipated starts --}}
+            <div class="most-anticipated w-1/4">
+                <h2 class="text-red-600 uppercase tracking-wide font-semibold">Most Anticipated</h2>
+                <div class="most-anticipated-container space-y-10 mt-8">
+                    <div class="game flex">
+                        <a href="">
+                            <img src="/assets/img/postar.jpg" alt="cover" class="w-16 hover:opacity-75 transition ease-in-out duration-150">
+                        </a>
+                        <div class="ml-4">
+                            <a href="#" class="hover:text-gray-300">NFS Most Wanted</a>
+                            <div class="text-gray-400 text-sm mt-1">November 11, 2005</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
+    {{-- end container --}}
 @endsection
