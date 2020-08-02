25cb84fb6a92bf7d3b34f3c2307759edfefadf1a
Rafid MD. Ahsan <rafidahsan698@gmail.com>
2020-08-02 19:24:49 +0600

    finishing single game page

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -107,9 +107,11 @@
                     @foreach ($game['similar_games'] as $game)
                     <div class="game mt-8">
                         <div class="relative inline-block">
-                            <a href="{{route('games.show', $game['slug'])}}">
-                                <img src="{{ Str::replaceFirst('thumb', 'cover_big', $game['cover']['url']) }}" alt="cover" class="hover:opacity-75 transition ease-in-out duration-150">
-                            </a>
+                            @if (array_key_exists('cover', $game))
+                                <a href="{{route('games.show', $game['slug'])}}">
+                                    <img src="{{ Str::replaceFirst('thumb', 'cover_big', $game['cover']['url']) }}" alt="cover" class="hover:opacity-75 transition ease-in-out duration-150">
+                                </a>
+                            @endif
                             @if (isset($game['rating']))
                                 <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px; bottom: -20px;">
                                     <div class="font-semibold text-xs flex justify-center items-center h-full">
